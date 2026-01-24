20cb45ce5b75a868fb0d09b209460721d8a0933a
amitsavani-45 <apsavani4518@gmail.com>
2026-01-24 17:11:30 +0530

    todo list for the atomone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import atomone from './image/atomone.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="container">
+        <img src={atomone}  alt="atomone-logo" className="logo"  />
+       
       </header>
-    </div>
+
+      <section className="section">
+         <h1>TODO APPLICATION FOR ATOMONE</h1>
+        <h1>Information of Employee</h1>
+      </section>
+   
+  <div className="form-container">
+  <div className="form-group">
+    <label>Name</label>
+    <input type="text" name="name" />
+  </div>
+
+  <div className="form-group">
+    <label>Age</label>
+    <input type="number" name="age" />
+  </div>
+
+  <div className="form-group">
+    <label>Department</label>
+    <input type="text" name="department" />
+  </div>
+
+  <div className="form-group">
+    <label>Job Role</label>
+    <input type="text" name="jobRole" />
+  </div>
+
+  <div className="form-group">
+    <label>Salary</label>
+    <input type="number" name="salary" />
+  </div>
+
+  <div className="form-group">
+    <label>Plant</label>
+    <input type="text" name="plant" />
+  </div>
+
+  <button type="submit">Submit Employee Data</button>
+</div>
+
+
+      </div>
+      
   );
+   
 }
 
 export default App;
